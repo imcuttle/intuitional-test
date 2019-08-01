a2ff74c239d5fb98ec2583ad9ad296e08f247e9f
imcuttle <moyuyc95@gmail.com>
2019-08-01 16:27:53 +0800

    chore: update

--- jest.config.base.js
+++ jest.config.base.js
@@ -7,7 +7,7 @@
 
 module.exports = {
   roots: ['<rootDir>', '<rootDir>/packages'],
-  testPathIgnorePatterns: ['__template', 'packages/.+/README.md', 'packages/.+/CHANGLOG.md'],
+  testPathIgnorePatterns: ['__template', 'packages/.+/README.md', 'packages/.+/CHANGLOG.md', 'CHANGELOG.md'],
   transform: {
     '^.+\\.jsx?$': '<rootDir>/packages/intuitional-test-babel-jest',
     '^.+\\.md$': '<rootDir>/packages/intuitional-test-babel-jest'
